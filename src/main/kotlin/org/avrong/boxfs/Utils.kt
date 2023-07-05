d0fda437a799aa77a9a00953f49707c198f668e1
Aleksei Trifonov <avrong@avrong.me>
2023-07-06 01:55:28 +0300

    Cleanup

--- src/main/kotlin/org/avrong/boxfs/Utils.kt
+++ src/main/kotlin/org/avrong/boxfs/Utils.kt
@@ -1,7 +1,3 @@
 package org.avrong.boxfs
 
-import java.nio.file.Path
-
 fun String.toBoxPath() = BoxPath(this)
-
-fun Path.toBoxPath() = BoxPath(this.toString())
--- src/main/kotlin/org/avrong/boxfs/population/PopulateFileVisitor.kt
+++ src/main/kotlin/org/avrong/boxfs/population/PopulateFileVisitor.kt
@@ -25,7 +25,6 @@ internal class PopulateFileVisitor(private val boxFs: BoxFs, private val prefix:
             println(e)
         }
 
-
         return super.visitFile(file, attrs)
     }
 
