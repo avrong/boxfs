434f50110e10e7642e732c8cadad68e3fe5e3750
Aleksei Trifonov <avrong@avrong.me>
2023-07-05 05:22:11 +0300

    Ignore .gradle and .git in functional test

--- src/test/kotlin/org/avrong/boxfs/FunctionalTest.kt
+++ src/test/kotlin/org/avrong/boxfs/FunctionalTest.kt
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import java.nio.file.Files
 import java.nio.file.Path
+import kotlin.io.path.listDirectoryEntries
+import kotlin.io.path.name
 import kotlin.test.assertEquals
 
 /**
@@ -41,24 +43,39 @@ class FunctionalTest {
         val boxFs = BoxFs.create(tempDir.resolve("project"))
         val workingDir = Path.of(System.getProperty("user.dir"))
 
-        // TODO: Currently there are a lot of small git files, maybe ignore .git completely?
         // Store all project trees
         val rootPath = BoxPath("/")
-        boxFs.populate(workingDir, rootPath)
 
+        // Populate first time. Ignore .gradle because of file lock, and .git because of big number
+        // small files
+        val ignoredDirs = setOf(".git", ".gradle").map { workingDir.resolve(it) }
+        val dirsToPopulate = workingDir.listDirectoryEntries().filterNot { ignoredDirs.contains(it) }
+        populateSelectedDirs(boxFs, dirsToPopulate)
+
+        // Remove 70% of files
         val fileCollector = FileCollector()
         boxFs.visitFileTree(rootPath, fileCollector)
         val files = fileCollector.allFiles
         val countFilesToRemove = (files.size * 0.7).toInt()
         files.shuffled().take(countFilesToRemove).forEach { boxFs.delete(it) }
 
-        boxFs.populate(workingDir, rootPath)
+        // Populate once again
+        populateSelectedDirs(boxFs, dirsToPopulate)
+
         val expectedTree = boxFs.getVisualTree(rootPath)
 
         // Compact
         boxFs.compact()
 
         assertEquals(expectedTree, boxFs.getVisualTree(rootPath))
+    }
+
+    fun populateSelectedDirs(boxFs: BoxFs, dirs: List<Path>) {
+        for (localDir in dirs) {
+            val dirName = "/".toBoxPath().with(localDir.name)
+            boxFs.createDirectory(dirName)
+            boxFs.populate(localDir, dirName)
+        }
     }
 
     class FileCollector : BoxFsVisitor {
